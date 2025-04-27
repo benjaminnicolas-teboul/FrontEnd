548a656d37e33b463de0b2f7f58ba97a26bff77a
benjaminnicolas-teboul <benjtenuts@hotmail.fr>
2025-04-27 11:16:10 +0100

    Delete FrontEnd-master directory

--- FrontEnd-master/assets/modulesJs/openModal.js
+++
@@ -1,9 +0,0 @@
-import {displayGalleryModale} from "./displayGalleryModale.js";
-import {modal} from "../script.js";
-
-export const openModal = (event) => {
-  const worksArray = JSON.parse(sessionStorage.getItem("works"));
-  event.preventDefault();
-  modal.style.display = "block";
-  displayGalleryModale(worksArray);
-};
